02d01879ad3a188488506c4f0518f04562942aa2
Begmatov Almaz <lolkek13@inbox.ru>
2024-10-15 18:35:58 +0600

    Добавил скролл сайд бара

--- src/Components/Countries/CountryList/CountryList.tsx
+++ src/Components/Countries/CountryList/CountryList.tsx
@@ -1,9 +1,11 @@
+import "./CountryList.css"
+
 interface CountriesProps {
   countries: string[];
 }
 const CountryList: React.FC<CountriesProps> = ({ countries }) => {
   return (
-    <div>
+    <div className="scroll-list">
       <ul className="list-group list-group-flush">
         {countries.map((name, index) => (
           <li className="list-group-item list-group-item-action" key={index}>
